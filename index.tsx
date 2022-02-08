c63c8f467db698907e16a45de52128348a4d9c23
shuangyue <18795905365@163.com>
2022-02-08 11:40:07 +0800

    feat: 1. 区块

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Demo from './src/Button'
+import './index.less'
+import 'antd/dist/antd.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Demo />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
